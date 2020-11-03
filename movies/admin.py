2839a223793bdcc9bc4fc3a5fd2f5e1ba0e850bc
Hussain Vaid <vaid@Mohmed.local>
2020-11-02 23:17:10 -0600

    final deployement

---
+++ movies/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Genre, Movie
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'numder_in_stock', 'daily_rates')
+    exclude = ('date_created',)
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
